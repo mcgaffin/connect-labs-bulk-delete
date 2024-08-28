2877306508b9b71378cecc7b1476491d97825471
David McGaffin <david.mcgaffin@posit.co>
2024-08-28 10:58:27 -0400

    everything works!

--- app.py
+++ app.py
@@ -1,75 +1,64 @@
+import json
 from shiny import reactive
 from shinyswatch import theme
 from shiny.express import input, render, ui
 from posit.connect import Client
 from posit.connect.errors import ClientError
 import pandas as pd
-import sys
 
 
-@reactive.effect(priority=100)
+@reactive.effect(priority=1)
 @reactive.event(input.delete_button)
 def on_delete():
-    print(results_df.cell_selection()["rows"])
     try:
         for item in results_df.cell_selection()["rows"]:
-            content[item].delete()
+            content()[item].delete()
             ui.notification_show(
-                f"Deleted {content[item]['title']}.", type="message", duration=3
+                f"Deleted {content()[item]['title']}.", type="message", duration=3
             )
     except ClientError as e:
         ui.notification_show(
-            f"Could not delete {content[item]['title']}: {e.error_message}",
+            f"Could not delete {content()[item]['title']}: {e.error_message}",
             type="error",
             duration=10,
         )
 
+def is_content_modified():
+    with Client(
+        api_key="ahF4EwonCpeHVLdByWjtCNmncGvWAc4o", url="http://localhost:3939"
+    ) as client:
+        find_results = client.content.find()
+        return hash(json.dumps(find_results))
 
-@reactive.effect(priority=1)
-@reactive.event(input.refresh_button)
-def on_refresh():
-    global content
-    print("Refresh")
-    content = client.content.find()
-
-
-content = None
-with Client(
-    api_key="ahF4EwonCpeHVLdByWjtCNmncGvWAc4o", url="http://localhost:3939"
-) as client:
-    content = client.content.find()
-
-# add_selected = lambda d: {**d, "selected": False}
-# selectable_content = list(map(add_selected, content))
+@reactive.poll(is_content_modified, 1)
+def content():
+    with Client(
+        api_key="ahF4EwonCpeHVLdByWjtCNmncGvWAc4o", url="http://localhost:3939"
+    ) as client:
+        find_results = client.content.find()
+        if len(input.search_term()) == 0:
+            return find_results
+        else:
+            return list(
+                filter(lambda c: input.search_term() in c["title"], find_results)
+            )
 
 
 ui.page_opts(title="Connect Explorer", theme=theme.flatly())
 with ui.layout_columns(col_widths=(12)):
+    with ui.card(fill=False):
+        "Actions"
+        ui.input_action_button("delete_button", "Delete", width="100px")
+
     with ui.card():
+
         ui.input_text("search_term", "Search", "")
-        ui.input_action_button("delete_button", "Delete")
-        ui.input_action_button("refresh_button", "Refresh")
-
-    with ui.card():
         "Content"
-
         @render.data_frame
         def results_df():
-            df = pd.DataFrame(
-                content, columns=["selected", "title", "last_deployed_time"]
-            )
+            df = pd.DataFrame(content(), columns=["title", "last_deployed_time"])
             return render.DataGrid(
                 df,
                 selection_mode="rows",
                 width="100%",
             )
-
-
-res = None
-
-
-@reactive.effect
-def _():
-    print(input.search_term())
-    res = client.content.find(title=input.search_term())
-    print(f"Search Result Count: { len(res) }")
